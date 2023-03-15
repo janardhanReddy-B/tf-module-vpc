c6c96e75182abb97fb1d992ccd0c703a3b775c11
JanardhanReddy <bommireddy246@gmail.com>
2023-03-15 03:00:09 +0000

    pipeline update shared library

--- gateways.tf
+++ gateways.tf
@@ -15,3 +15,15 @@ resource "aws_nat_gateway" "NAT" {
     Name = "${var.PROJECT}-${var.ENV}-NAT"
   }
 }
+
+resource "aws_route" "ig-route-public-subnet" {
+  route_table_id            = aws_route_table.public.id
+  destination_cidr_block    = "0.0.0.0/0"
+  gateway_id = aws_internet_gateway.IG.id
+}
+
+resource "aws_route" "nat-route-private-subnet" {
+  route_table_id            = aws_route_table.private.id
+  destination_cidr_block    = "0.0.0.0/0"
+  nat_gateway_id = aws_nat_gateway.NAT.id
+}
