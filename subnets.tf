242437ebe290e91f0aaa1b09bdf431eeeb00a4c2
JanardhanReddy <bommireddy246@gmail.com>
2023-03-15 02:38:23 +0000

    pipeline update shared library

--- subnets.tf
+++ subnets.tf
@@ -1,4 +1,5 @@
 resource "aws_subnet" "public" {
+  count             = length(var.PUBLIC_SUBNETS_CIDR)
   vpc_id     = aws_vpc.main.id
   cidr_block = var.PUBLIC_SUBNETS_CIDR[count.index]
   availability_zone = var.AZ[count.index]
@@ -9,6 +10,7 @@ resource "aws_subnet" "public" {
 }
 
 resource "aws_subnet" "private" {
+  count             = length(var.PRIVATE_SUBNETS_CIDR)
   vpc_id     = aws_vpc.main.id
   cidr_block = var.PRIVATE_SUBNETS_CIDR[count.index]
   availability_zone = var.AZ[count.index]
